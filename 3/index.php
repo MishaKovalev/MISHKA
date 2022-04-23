511492dfcc9b85a7cb0a6fe2917d0cca74bd2be7
MishaKovalev <90443727+MishaKovalev@users.noreply.github.com>
2022-04-23 07:23:51 +0300

    Update index.php

--- 3/index.php
+++ 3/index.php
@@ -1,55 +1,48 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 
-
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    
     if (!empty($_GET['save'])) {
-        
         print('Спасибо, результаты сохранены.');
     }
-    
     include('form.php');
-   
     exit();
 }
-
 $errors = FALSE;
 if (empty($_POST['name'])) {
-    print('Заполните имя.<br/>');
+    print('Заполни имя.<br/>');
     $errors = TRUE;
 }
 if (empty($_POST['email'])) {
-    print('Заполните email.<br/>');
+    print('Заполни email.<br/>');
     $errors = TRUE;
 }
 if (empty($_POST['date'])) {
-    print('Выберите дату.<br/>');
+    print('Выбери дату.<br/>');
     $errors = TRUE;
 }
 if (empty($_POST['gender'])) {
-    print('Выберите пол.<br/>');
+    print('Выбери пол.<br/>');
     $errors = TRUE;
 }
 if (empty($_POST['parts'])) {
-    print('Выберите количество конечностей.<br/>');
+    print('Выбери количество конечностей.<br/>');
     $errors = TRUE;
 }
 if (empty($_POST['powers'])) {
-    print('Выберите суперспособнос(ть/ти).<br/>');
+    print('Выбери суперспособнос(ть/ти).<br/>');
     $errors = TRUE;
 }
 if (empty($_POST['bio'])) {
-    print('Расскажите о себе.<br/>');
+    print('Расскажи о себе.<br/>');
     $errors = TRUE;
 }
 if (empty($_POST['policy'])) {
-    print('Ознакомтесь с политикой обработки данных.<br/>');
+    print('Ознакомся с политикой обработки данных.<br/>');
     $errors = TRUE;
 }
 
 if ($errors) {
-    // При наличии ошибок завершаем работу скрипта.
     exit();
 }
 
@@ -59,25 +52,23 @@ $email = $_POST['email'];
 $date = $_POST['date'];
 $gender = $_POST['gender'];
 $parts = $_POST['parts'];
+$bio = $_POST['bio'];
 $policy = $_POST['policy'];
-$powers = implode(',',$_POST['powers']);
+$powers = implode(',', $_POST['powers']);
 
 $user = 'u47529';
 $pass = '5988897';
 $db = new PDO('mysql:host=localhost;dbname=u47529', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 
-// Подготовленный запрос. Не именованные метки.
 try {
-  $stmt = $db->prepare("INSERT INTO users SET name = ?, email = ?, date = ?, gender = ?, parts = ?, bio = ?, policy = ?");
-  $stmt->execute(array($name, $email, $date, $gender, $parts, $bio, $policy));
-  $power_id = $db->lastInsertId();
-  
-  $superpowers = $db->prepare("INSERT INTO superpowers SET powers = ?, userID = ?");
-  $superpowers -> execute(array($powers, $power_id));
-}
-catch(PDOException $e){
-  print('Error : ' . $e->getMessage());
-  exit();
-}
+    $stmt = $db->prepare("INSERT INTO members SET name = ?, email = ?, date = ?, gender = ?, parts = ?, bio = ?, policy = ?");
+    $stmt->execute(array($name, $email, $date, $gender, $parts, $bio, $policy));
+    $user_id = $db->lastInsertId();
 
+    $superpowers = $db->prepare("INSERT INTO superpowers SET powers = ?, userID = ? ");
+    $superpowers->execute(array($powers, $user_id));
+} catch (PDOException $e) {
+    print('Error : ' . $e->getMessage());
+    exit();
+}
 header('Location: ?save=1');
